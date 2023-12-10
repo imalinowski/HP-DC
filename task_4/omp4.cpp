c3801a2e9a78b929fdd0e952721e00307d2ab8a0
Ilya Malinowski <malinowski221106@gmail.com>
2023-12-10 11:57:09 +0300

    task_4 fixed

--- task_4/omp4.cpp
+++ task_4/omp4.cpp
@@ -17,65 +17,48 @@ int** generateRandomMatrix(int size) {
   return matrix;
 }
 
-int* generateRandomArray(int size) {
-  int* a = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        a[i] = rand() % 11 - 5;
+int matrixSize = 1000; // 1_000
+
+int findMinOfArray(int* array) {
+  int minRowValue = 99999;
+  for (int j = 0; j < matrixSize; ++j)
+  {
+    if (array[j] < minRowValue) {
+      minRowValue = array[j];
     }
-  return a;
+  }
+  return minRowValue;
 }
 
+int** matrix = generateRandomMatrix(matrixSize);
+
 void calcMaxMinMatrix(int threads) {
-  int matrixSize = 100;
-  int** matrix = generateRandomMatrix(matrixSize);
-
-  srand(static_cast<unsigned int>(time(0)));
-
-  int chunkSize = matrixSize / threads;
   omp_set_num_threads(threads);
 
-  printf("ChunkSize = %d, Threads = %d\n", chunkSize, threads);
+  int maxValue = 0;
+  int chunkSize = matrixSize / threads;
 
   double time_start = omp_get_wtime();
 
-  int maxValue = 0;
-
   #pragma omp parallel shared(matrix, maxValue)
   {
-    #pragma omp for schedule(dynamic, chunkSize)
+    #pragma omp for schedule(dynamic, chunkSize) reduction(max:maxValue)
       for (int i = 0; i < matrixSize; ++i) {
-        int minRowValue = 9999;
-
-        for (int j = 0; j < matrixSize; ++j)
-        {
-          if (matrix[i][j] < minRowValue)
-          {
-            minRowValue = matrix[i][j];
-          }
-        }
-
-        if (minRowValue > maxValue)
-        {
-          #pragma omp critical
-          if (minRowValue > maxValue)
-          {
-            maxValue = minRowValue;
-          }
-        }
+        int minRowValue = findMinOfArray(matrix[i]);
+        maxValue = minRowValue;
       }
   }
 
   double delta = (omp_get_wtime() - time_start);
 
-  printf("time: %f\n", delta);
+  printf("time: %f threads = %d\n", delta, threads);
 }
 
 int main()
 {
   int threads = 1;
 
-  for (int i = threads; i <= 10; i++){
+  for (int i = threads; i <= 7; i++){
     calcMaxMinMatrix(threads);
     threads += 1;
   }
