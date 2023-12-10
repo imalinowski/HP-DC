db97761c61a881ed863e3a925f9d29dc179d3341
Ilya Malinowski <malinowski221106@gmail.com>
2023-12-10 23:52:37 +0300

    task_7 fixed

--- task_7/omp7.cpp
+++ task_7/omp7.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <ctime> 
+#include <iostream>
 
 int** generateRandomMatrix(int size) {
   int** matrix = new int*[size];
@@ -17,36 +18,63 @@ int** generateRandomMatrix(int size) {
   return matrix;
 }
 
+int arraySize = 1000; // 1_000
+
 void scalarMulty(int threads) {
   omp_set_num_threads(threads);
 
-  int arraySize = 100;
-  int** arrays = generateRandomMatrix(arraySize);
-  int scalarVectorMult = 0;
+  int** arrays = new int*[arraySize];
+  int* vectorMult = new int[arraySize];
+  for( int i = 0; i < arraySize; ++i) {
+    vectorMult[i] = 1;
+  }
+  int* scalarMult = 0;
+  int currentGeneredLine = 0;
 
+  omp_lock_t lock;
+  omp_init_lock(&lock);
   double time_start = omp_get_wtime();
-  #pragma omp parallel sections reduction(+:scalarVectorMult)
+  #pragma omp parallel sections
     {
         #pragma omp section
         {    
-            for (int i = 1; i < arraySize; ++i) {
+            for (int i = 0; i < arraySize; ++i) {
+              omp_set_lock(&lock);
+              int* a = new int[arraySize];
               for (int j = 0; j < arraySize; ++j) {
-                scalarVectorMult += arrays[i-1][j] * arrays[i][j];
+                a[j] = i * j;
+              }
+              arrays[i] = a;
+              currentGeneredLine = i;
+              omp_unset_lock(&lock);
+            }
+        }
+        #pragma omp section
+        {    
+            for (int i = 1; i < arraySize; ++i) {
+              if(currentGeneredLine < i) {
+                  omp_set_lock(&lock);
+                  omp_unset_lock(&lock);
+              }
+              for (int j = 0; j < arraySize; ++j) {
+                vectorMult[j] *= arrays[i][j];
               }
             }
-
+            for (int i = 0; i < arraySize; ++i) {
+              scalarMult += vectorMult[i];
+            }
         }
     }
 
   double delta = (omp_get_wtime() - time_start);
-  printf("Threads = %d Time = %f \n", threads, delta);
+  printf("time: %f threads = %d\n", delta, threads);
 }
 
 int main()
 {
   int threads = 1;
 
-  for (int i = threads; i <= 10; i++){
+  for (int i = threads; i <= 7; i++){
     scalarMulty(threads);
     threads += 1;
   }
